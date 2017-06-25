615341cfacf5bce1fbc75ad0d9d818aa47b580bb
olegbezkorovanynyi <oleg.bezkorovaynyi@gmail.co>
2017-06-25 04:08:58 +0300

    cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import {CustomValidators} from "./custom.validators";
     styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-    projectForm:FormGroup;
+    projectForm: FormGroup;
 
     ngOnInit() {
         this.projectForm = new FormGroup({
